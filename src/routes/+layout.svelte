7e7661dfda2583cb765e6d36688fe2aa1c48d6ed
Austin Corwin <austincorwin5@gmail.com>
2024-11-20 15:46:34 -0700

    General sveltekit project setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<nav class="flex w-full justify-between">
+	<a href="/">Home</a>
+	<div>Austin Corwin</div>
+	<a href="/settings">Settings</a>
+</nav>
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<p>oh hey there,</p>
+<h1>I'm Austin Corwin</h1>
+<a href="/about">About my site</a>
+
+<h2>Bike Writer 🚲 and Code Rider 👨‍💻</h2>
+
+Fun facts: I've ridden over xxx miles on my Bike I have 4 singles released on spotify
+<button>Let's connect!</button>
